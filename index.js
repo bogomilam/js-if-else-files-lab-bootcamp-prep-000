29166694fdf06957e056eab95a1cfefcfc5e675f
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-04 09:19:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
+if (height === "S70") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected height to be 70")
+}
 // Don't worry about this
 module.exports = { name, height
 }
